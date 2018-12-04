3153a4c5eb5bb487d24a7da40a4ff980126527e9
George Smerin <george.smerin@flippercommunity.com>
2018-12-04 17:46:32 +0000

    Remove unecessary source config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "pages",
-        path: `${__dirname}/src/pages`
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
         name: "images",
         path: `${__dirname}/src/images/`
       }
